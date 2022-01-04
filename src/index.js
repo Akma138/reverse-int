9bf30c41fde3fcb21c48aeb0fa288857b2778af8
Akma <napoli-kz@mail.ru>
2022-01-04 16:24:26 +0200

    feat:code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0) {
+    n = n * (-1);
+    return n.toString().split('').reverse().join('');
+  }
+  let z = n.toString();
+  return z.split('').reverse().join('');
 }
